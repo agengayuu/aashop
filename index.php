8d87536a20eb99b6c07f162c9748fd65431fcb1f
agengayuu <ayuageng@apps.ipb.ac.id>
2022-01-23 12:02:57 +0700

    add base url

--- index.php
+++ index.php
@@ -7,4 +7,32 @@
     <head>
         <title>INDEX AASHOP</title>
     </head>
+
+    <body>
+        <div id="container">
+            <div id="header">
+                <a href="<?php echo BASE_URL."index.php"; ?>">
+                    <img src="<?php echo BASE_URL."images/logo.png"; ?>" alt="">
+                </a>
+
+                <div id="menu">
+                    <div id="user">
+                        <a href="<?php echo BASE_URL."index.php?page=login"; ?>">Login</a>
+                        <a href="<?php echo BASE_URL."index.php?page=register"; ?>">Register</a>
+                    </div>
+
+                    <a href="<?php echo BASE_URL."index.php?page=keranjang"; ?>">
+                    <img src="<?php echo BASE_URL."images/cart.png"; ?>" />
+                    </a>
+                </div>
+            </div>
+
+            <div id="content"></div>
+
+            <div id="footer">
+                <p>Copyright aashop 2022</p>
+            </div>
+            
+        </div>  
+    </body>
 </html>
