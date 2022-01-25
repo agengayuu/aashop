c91c576bdb87eddbc2303a6128644f56cff523d0
agengayuu <ayuageng@apps.ipb.ac.id>
2022-01-25 23:01:32 +0700

    Add page in index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
     include_once("function/helper.php");
+
+    $page = isset($_GET['page']) ? $_GET['page'] : false;
+
 ?>
 
 <!DOCTYPE html>
@@ -29,7 +32,17 @@
                 </div>
             </div>
 
-            <div id="content"></div>
+            <div id="content">
+                <?php 
+                    $filename = "$page.php";
+
+                    if(file_exists($filename)) {
+                        include_once($filename);
+                    }else{
+                        echo "Maaf, tidak ada laman";
+                    }
+                ?>
+            </div>
 
             <div id="footer">
                 <p>Copyright aashop 2022</p>
