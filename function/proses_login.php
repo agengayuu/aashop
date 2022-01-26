9d8f197bf1dd0081f323a647c6f8404f8be9853c
agengayuu <ayuageng@apps.ipb.ac.id>
2022-01-26 22:28:45 +0700

    Add Session Login

--- function/proses_login.php
+++ function/proses_login.php
@@ -12,5 +12,13 @@
         header("location:". BASE_URL . "index.php?page=login&notif=true");
     } else {
         $row = mysqli_fetch_assoc($query);
+
+        session_start();
+
+        $_SESSION['user_id'] = $row['user_id'];
+        $_SESSION['nama']    = $row['nama'];
+        $_SESSION['level']   = $row['level'];
+
+        header("location:" . BASE_URL ."index.php?page=./content/my_profile&module=pesanan&action=list");
     }
 ?>
